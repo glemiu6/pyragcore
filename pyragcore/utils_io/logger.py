4426b1c751f52390516536d9b8ec539de0cf1fe7
glemiu6 <digorivlad59@gmail.com>
2026-04-28 03:29:44 +0100

    logger

--- pyragcore/utils_io/logger.py
+++ pyragcore/utils_io/logger.py
@@ -1,8 +1,15 @@
 import logging
 
-def set_up_logging(level=logging.INFO):
-    logging.basicConfig(level=level,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def get_logger(name):
-    return logging.getLogger(name)
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)  # change to WARNING for production
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
